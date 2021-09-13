b1a0dd68ee2bd1dca8108fd1f8353c4baa197358
Daniel Velasquez <d.velasquezo@uniandes.edu.co>
2021-09-12 23:40:43 -0500

    Actualizando Modelo de datos Historia Clinica

--- historia_clinica/modelos/modelos.py
+++ historia_clinica/modelos/modelos.py
@@ -7,9 +7,22 @@ db = SQLAlchemy()
 class Paciente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(128))
+    eventos = db.relationship('EventosPaciente', cascade='all, delete, delete-orphan')
+
+class EventosPaciente(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.String(128))
+    evento = db.Column(db.String(128))
+    paciente = db.Column(db.Integer, db.ForeignKey("paciente.id"))
 
 class PacienteSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Paciente
+        include_relationships = True
+        load_instance = True
+
+class EventoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = EventosPaciente
         include_relationships = True
         load_instance = True
