dfd05ee05b92c8befb2a903031a5a3f1fc273732
Juan José Chiroque <j.chiroque@uniandes.edu.co>
2021-10-03 21:27:06 -0500

    add api gateway

--- apiGateway/app.py
+++ apiGateway/app.py
@@ -1,57 +1,31 @@
-import threading
-from typing import Counter
-from requests import status_codes
-from werkzeug.wrappers import response
-from apiGateway import create_app
-from flask_restful import Resource, Api
-from flask import Flask, app, request, jsonify
-from random import random
+from flask import Flask, jsonify, request
 import requests
-import json
-from datetime import datetime
-import time
-from threading import Thread
 
+app = Flask(__name__)
 
-app = create_app('default')
-app_context = app.app_context()
-app_context.push()
+@app.route("/", methods=["GET"])
+def prueba1(): 
+  return jsonify({"message": "success"})
 
-api = Api(app)
+@app.route("/jwt", methods=["GET"])
+def getToken(): 
+  res = requests.post('http://localhost:5001/token')
+  return res.json(), res.status_code
 
-class VistaRegistro(Resource):
+@app.route("/pacientes", methods=["POST"])
+def create_paciente():   
+  headers = {'Authorization': request.headers.get('Authorization')}   
+  data = request.json; 
+  res = requests.post('http://localhost:5002/pacientes', json=data, headers=headers)
+  return res.json(), res.status_code
 
-    def post(self, id_paciente):    
-        paciente = {}              
-        paciente['evento'] = request.json['evento']
-        paciente['fecha'] = request.json['fecha']
-        args = (paciente,)
-        payload = {'evento' : paciente['evento'], 'fecha':paciente['fecha']}
-        r = requests.post('http://127.0.0.1:5002/paciente/{}/registrarevento'.format(id_paciente), json=payload)
-        return r.text
+@app.route("/paciente/<paciente_id>", methods=["GET"])
+def get_paciente(paciente_id):
+  print('http://localhost:5002/paciente/{}'.format(paciente_id))
+  headers = {'Authorization': request.headers.get('Authorization')}    
+  res = requests.get('http://localhost:5002/paciente/{}'.format(paciente_id), headers=headers)  
+  return res.json(), res.status_code
 
-class VistaMonitor(Resource):
-    def post(self, pto_microservicio):
-        def call_monitor(port):
-            ping = int(random())
-            endpoint_url = 'http://127.0.0.1:' + str(pto_microservicio) +'/ping/{}'.format(ping)
-            counter = 0
-            while counter < 99:
-                try:
-                    counter += 1
-                    res = requests.post(endpoint_url)
-                    print("ping ok, ", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-                    time.sleep(5)
-                except Exception as err:
-                    print("connection failed, ", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-                    time.sleep(5)
-
-        port_ms = 5003
-        thread = Thread(target=call_monitor, kwargs={'port':port_ms})
-        thread.start() 
-
-        return "Monitor activado"
-
-api.add_resource(VistaRegistro,'/paciente/<int:id_paciente>/registrar')
-api.add_resource(VistaMonitor,'/monitor/<int:pto_microservicio>/check')
-
+if __name__ == '__main__':
+  app.run(debug=True, port=5000)
+  
